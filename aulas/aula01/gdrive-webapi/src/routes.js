49ca845d2f8a58b616390d99eeb600abe5524bb8
Thiago <tms1_tms2@hotmail.com>
2021-09-09 11:30:44 -0300

    Socket and routes schema

---
+++ aulas/aula01/gdrive-webapi/src/routes.js
@@ -0,0 +1,43 @@
+import { logger } from "./logger.js";
+
+export default class Routes {
+  io;
+  constructor() {
+
+  }
+
+  setSocketInstance(io) {
+    this.io = io;
+  }
+
+  async defaultRoute(request, response) {
+    response.end("Hello World");
+  }
+
+  // Enviado pelo navegador pra saber se a api está de pé
+  async options(request, response) {
+    response.writeHead(204);
+    response.end("Hello World");
+  }
+
+  async post(request, response) {
+    logger.info("post");
+    response.end();
+  }
+
+  async get(request, response) {
+    logger.info("get");
+    response.end();
+  }
+
+  handler(request, response) {
+    // Liberar o cors
+    response.setHeader("Access-Control-Allow-Origin", "*");
+
+    // POST, GET
+    const chosen = this[request.method.toLowerCase()] || this.defaultRoute;
+
+    // Apply equivale a chosen(). A diferença é a atribuição do this, que se refere a toda a classe. O segundo parâmetro é o array de argumentos.
+    return chosen.apply(this, [request, response]);
+  }
+}
